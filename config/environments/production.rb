9087d76b0ba67c574543b9785b00a33a0941d715
DAVID RUIZ DE LA ROSA <ddlarosa@thesensorylab.es>
2018-09-26 12:02:08 +0200

    Set img tag true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
 
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
@@ -25,9 +27,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
