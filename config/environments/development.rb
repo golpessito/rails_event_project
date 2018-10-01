fdbe4a2faf9455c2f284561e5cf8cb334745bde4
DAVID RUIZ DE LA ROSA <ddlarosa@thesensorylab.es>
2018-10-01 11:07:08 +0200

    Paper Clip With S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Use to authenticate users
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -64,4 +64,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  
 end
