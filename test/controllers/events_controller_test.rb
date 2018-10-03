481c7956b0ab8a3a178be30328909168a3ae4357
David Ruiz <david.ruizdelarosa@gmail.com>
2018-10-03 19:02:51 +0200

    fixt test in production 2

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -9,18 +9,6 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
      @photo = File.new(ruby_path,'rb')
      @user = users(:david)
      @author = users(:author)
-
-     @event = Event.new(
-       name: "Madrid Rio4",
-       address: "C/Florida, Aranjuez, Madrid",
-       start_at: "2018-09-01 11:14:17",
-       end_at: "2018-09-01 12:14:17",
-       photo_file_name: "florida.jpg",
-       description: "This is the best music event ever ...",
-       latitude: 40.0321293,
-       longitude: -3.6028448,
-       user: @author,
-       photo: @photo)
   end
 
   test "user can see home page whitout login" do
@@ -44,21 +32,23 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
    end
 
    test "should not create event without login" do
-      post events_url, params: { event: { address: @event.address, description: @event.description, end_at: @event.end_at, photo_file_name: @event.photo_file_name, name: @event.name, start_at: @event.start_at } }
+     my_event = Event.new(
+       name: "Madrid Rio4",
+       address: "C/Florida, Aranjuez, Madrid",
+       start_at: "2018-09-01 11:14:17",
+       end_at: "2018-09-01 12:14:17",
+       photo_file_name: "florida.jpg",
+       description: "This is the best music event ever ...",
+       latitude: 40.0321293,
+       longitude: -3.6028448,
+       user: @author,
+       photo: @photo)
+
+      post events_url, params: { event: { address: my_event.address, description: my_event.description, end_at: my_event.end_at, photo_file_name: my_event.photo_file_name, name: my_event.name, start_at: my_event.start_at } }
       follow_redirect!
       assert_equal "/", path
       assert_equal 200, status
    end
 
-   #Maybe check if submot button is disabled when dont login
-   test "should be attendee" do
-      @my_event=events(:madrid_rio)
-      login_as(@user)
-      get "/events/#{@my_event.id}"
-      assert_response :success
-      #post rsvps_url, params: { event: { address: @event.address, description: @event.description, end_at: @event.end_at, photo_file_name: @event.photo_file_name, name: @event.name, start_at: @event.start_at } }
-      #follow_redirect!
-      Warden.test_reset! #reset Warden after each example
-   end
 
 end
